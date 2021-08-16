12f4cb2e1d68f6933bce0ed3d3a44fcb52c64cec
hexiaoyu <xiaoyu.he@capitalonline.net>
2021-08-16 14:35:47 +0800

    '最长回文字串解法一'

--- test.py
+++ test.py
@@ -1,59 +1,98 @@
-class Solution(object):
-    def get_min(self,data1,data2):
-        if data1< data2:
-            return data1
-        else:
-            return data2
+class Solution:
+    def longestPalindrome(self, strs):
+        '''
+        维护四个指针，left、left1、right、right1,需要知道mid位置，个数为奇偶数
+        越界：
+        left1<left
+        '''
+        s_len = len(strs)
+        if len(set(list(strs)))==1:
+            return strs
 
-    def get(self,nums1,nums2,mid):
-        # 对于长度为0的列表处理
-        mid_2=mid//2
-        a_index=min(mid_2-1,len(nums1)-1)
-        b_index=min(mid_2-1,len(nums2)-1)
-        print('nums1:mid_2:{}--a_index:{}--nums1_len{}'.format(mid_2,a_index,len(nums1)))
-        print('nums2:mid_2:{}--b_index:{}--nums2_len{}'.format(mid_2,b_index,len(nums2)))
+        for str_len in range(s_len,0,-1):#字串长度从长到短：
+            for index in range(s_len):#子串的起始位置
+                if index+str_len>s_len:
+                    break
+                child_str = strs[index:index+str_len]
+                child_str_len = len(child_str)
+                if child_str_len==1:
+                    return child_str
+                
+                mid = child_str_len//2
+                if child_str_len%2==0:
+                    #子串为偶数
+                    left = 0
+                    right = mid-1                    
+                    left1 = mid
+                    right1 =  child_str_len-1
 
-        if len(nums1)==0:
-            return nums2[mid-1]
+                else:
+                    left = 0
+                    right = mid-1
+                    left1 = mid+1
+                    right1 =  child_str_len-1
 
-        if len(nums2)==0:
-            return nums1[mid-1]
+                while left<= right:
+                    # print(child_str,left,right,left1,right1)
+                    if child_str[left]!=child_str[right1] or child_str[left1]!=child_str[right]:
+                        break
+                    # print('{}---{}'.format(left,right))
+                    if left==right or left+1 == right:
+                        # print('====找到一个值为：{}===='.format(child_str))
+                        return child_str
+                    left+=1
+                    left1+=1
+                    right-=1
+                    right1-=1
+                
 
-        if mid==1:
-            return self.get_min(nums1[0],nums2[0])
-        
-        if nums1[a_index] <=nums2[b_index]:
-            return self.get(nums1[a_index+1:],nums2,mid-(a_index+1))
-        elif nums1[a_index] >nums2[b_index]:
-            return self.get(nums1,nums2[b_index+1:],mid-(b_index+1))
 
-    def findMedianSortedArrays(self, nums1, nums2):
-        nums1_len=len(nums1)
-        nums2_len=len(nums2)
-        mid=(nums1_len+nums2_len)//2
-        right =  self.get(nums1,nums2,mid+1)
-        print('right：{}'.format(right))
-        if (nums1_len+nums2_len)%2==0:
-            left =self.get(nums1,nums2,mid)
-            print('left：{}'.format(left))
-            res=(left+right)/2
-        else:
-            res=right
-        return res
-        
-
-        
+               
 
 if __name__ == '__main__':
-    Solution=Solution()
-    #2 2.5 0 1 2
-    # nums1,nums2=[1,3],[2]
-    # nums1,nums2=[1,2],[3,4]
-    # nums1,nums2=[0,0],[0,0]
-    # nums1,nums2=[],[1]
-    # nums1,nums2=[2],[]
-    nums1,nums2=[1],[2,3,4,5,6]
-    # nums1,nums2 =[4],[1,2,3,5,6]
+    # strs='babad'
+    # strs="ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg"
+    # strs = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    # strs = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    # strs = "aacabdkacaa"
+    # strs="cbbd"
+    # strs='ac'
+    # strs = "abcba"
+    solution=Solution()
+    res = solution.longestPalindrome(strs)
+    print('====找到值为：{}===='.format(res))
 
-    res=Solution.findMedianSortedArrays(nums1,nums2)
-    print(res)
+
+
+
+
+# class Solution:
+#     def longestPalindrome(self, strs):
+#         '''
+#         维护四个指针，left、left1、right、right1,需要知道mid位置，个数为奇偶数
+#         越界：
+#         left1<left
+#         '''
+
+#         res=''
+#         s_len = len(strs)
+#         if len(set(list(strs)))==1:
+#             return strs
+            
+#         for str_len in range(s_len,0,-1):#字串长度从长到短：
+#             for index in range(s_len):#子串的起始位置
+#                 if index+str_len>s_len:
+#                     break
+
+#                 child_str = strs[index:index+str_len]
+#                 # print('当前字串的起始位置：{},结束位置：{},当前字串为：{}'.format(index,index+str_len,child_str))
+#                 left = 0
+#                 right =len(child_str)-1
+#                 while child_str[left] == child_str[right] and left<=right:
+#                         # print('进入数据判断左侧值为{}--坐标为{}--右侧值为{}--坐标为{}'.format(child_str[left],left,child_str[right],right))
+#                         if left==right or left+1==right:
+#                             res = child_str
+#                             return res
+#                         left+=1
+#                         right-=1
+#                         # print(left,right)
