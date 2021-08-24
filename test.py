58229e3f4b2ee6d9c20e84f78ac5c6671e3db202
hexiaoyu <xiaoyu.he@capitalonline.net>
2021-08-24 18:52:11 +0800

    '整数反转初版'

--- test.py
+++ test.py
@@ -1,28 +1,32 @@
-class Solution(object):
-    def convert(self, s, numRows):
-        if numRows==1:
-            return s
-        res_list =['' for x in range(numRows)]
-        index,flag=0,-1
-        for char in s:
-            res_list[index]+=char
-            if flag==1:
-                print('向下字符'+char)
-            else:
-                print('向上字符'+char)
-            if index==0 or index==numRows-1: flag =- flag
-            index+=flag
-            
-        return ('').join(res_list) 
+class Solution:
+    def reverse(self, x):
+        '''
+        -2^31 <= x <= 2^31 - 1
+        动态规划
+        '''
+        if len(str(x))==1:
+            return x
+
+        data_str=str(x)
+        op=''
+        if data_str[0] in ['+','-']:
+            op = data_str[0]
+            data_str=data_str[1:]
+
+        data_str_len=len(data_str)
+        mid=data_str_len//2# 向下取整
+        #数量为偶数
+        left_str = data_str[:mid]
+        mid_str = data_str[mid] if data_str_len%2!=0 else ''
+        right_str = data_str[mid:]  if data_str_len%2!=0 else data_str[mid+1:] 
+        left_str_reverse = self.reverse(left_str)
+        right_str_reverse = self.reverse(right_str)
+        return op+right_str_reverse+mid_str+left_str_reverse
 
 if __name__ == '__main__':
-    numRows=2
-    # strs='PAYPALISHIRING'
-    # strs='PAYPALISHIRING'
-    strs="ABCDEF"
-
+    data=123
     solution = Solution()
-    res = solution.convert(strs,numRows)
+    res = solution.reverse(data)
     print(res)
 
 
