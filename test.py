954e2698d0768f11a7520612469fe9a3eec76eee
hexiaoyuchina <1642567075@qq.com>
2021-09-01 20:58:25 +0800

    '回文自判断不将数字转换成字符串'

--- test.py
+++ test.py
@@ -1,49 +1,19 @@
 class Solution:
     def isPalindrome(self, x):
-        if x<0:
+        if x<0 or x%10 == 0 and x!=0:
             return False
-        data_str = str(x)
-        data_str_len = len(data_str)
-        if data_str_len==1:
-            print('--')
+        revert_number = 0
+
+        while x>revert_number:
+          revert_number =revert_number*10 + x%10
+          x =x//10
+          print('x={},revert_number={}'.format(x,revert_number))
+        if x==revert_number or x ==revert_number//10:
             return True
-        if data_str_len==2:
-            if data_str[0]==data_str[1]:
-                rint('--')
-                return True
-            else:
-                return False
-
-        left_start = 0
-        right_end= data_str_len-1
-        mid = data_str_len//2
-        if data_str_len%2 ==0:
-            left_end = mid
-            right_start = mid+1
-        else:
-            left_end = mid-1
-            right_start = mid+1
-
-        print('left_start：{}，left_end：{}，right_start：{}, right_end：{}'.format(left_start,left_end,right_start,right_end))
-        while left_start<=left_end:
-            if data_str[left_start] == data_str[right_end] and data_str[left_end] == data_str[right_start]:
-                if left_start == left_end:
-                    return True
-                left_start+=1
-                left_end-=1
-                right_start+=1
-                right_end-=1
-            else:
-                return False
-        print(left_end,left_start)
-        if left_end+1 == left_start:
-            return True
-        
-
-        
+        return False
 
 if __name__ == '__main__':
-    data=1001
+    data=1010
     solution = Solution()
     res = solution.isPalindrome(data)
     print(res)
