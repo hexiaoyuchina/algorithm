8ad585efa1eae7ffaac7c6dd53b77ab2da550fbd
hexiaoyu <xiaoyu.he@capitalonline.net>
2021-09-01 19:11:01 +0800

    '回文字判断字符串方法'

--- test.py
+++ test.py
@@ -1,42 +1,50 @@
 class Solution:
-    def myAtoi(self, strs):
-        front = 1 # 前导数据判断
-        res_str = '+0'
-        for char in strs:
-            print('当前字符:***{}***'.format(char))
-            # 前导空格去除
-            if char ==' ' and front:
-                print('空格跳过')
-                continue
-
-            # 数据正负
-            if char in ['+','-'] and front:
-                front = 0
-                print('获取到正负号')
-                res_str = char+'0'
-                continue
-
-            if char not in list('0123456789') and front:
-                return 0
-            elif char not in list('0123456789'):
-                print('找的不是数字，终止了')
-                return int(res_str)
-                break
+    def isPalindrome(self, x):
+        if x<0:
+            return False
+        data_str = str(x)
+        data_str_len = len(data_str)
+        if data_str_len==1:
+            print('--')
+            return True
+        if data_str_len==2:
+            if data_str[0]==data_str[1]:
+                rint('--')
+                return True
             else:
-                front = 0
-                res_str+=char
+                return False
 
-            if int(res_str)<-2147483648:
-                print('当前数据小于范围数据，返回最小值')
-                return  -2147483648
-            if int(res_str)>2147483647:
-                print('当前数据大于范围数据，返回最大值')
-                return 2147483647
+        left_start = 0
+        right_end= data_str_len-1
+        mid = data_str_len//2
+        if data_str_len%2 ==0:
+            left_end = mid
+            right_start = mid+1
+        else:
+            left_end = mid-1
+            right_start = mid+1
 
-        return int(res_str)
+        print('left_start：{}，left_end：{}，right_start：{}, right_end：{}'.format(left_start,left_end,right_start,right_end))
+        while left_start<=left_end:
+            if data_str[left_start] == data_str[right_end] and data_str[left_end] == data_str[right_start]:
+                if left_start == left_end:
+                    return True
+                left_start+=1
+                left_end-=1
+                right_start+=1
+                right_end-=1
+            else:
+                return False
+        print(left_end,left_start)
+        if left_end+1 == left_start:
+            return True
+        
+
+        
+
 if __name__ == '__main__':
-    strs= "-91283472332"
+    data=1001
     solution = Solution()
-    res = solution.myAtoi(strs)
+    res = solution.isPalindrome(data)
     print(res)
 
